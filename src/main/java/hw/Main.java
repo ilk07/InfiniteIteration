b4ef6de8faadf3bbd215a80c19f0aadc1d41f996
ilk07 <ilk07@inbox.ru>
2023-02-08 20:00:22 +0300

    initial commit

---
+++ src/main/java/hw/Main.java
@@ -0,0 +1,14 @@
+package hw;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        for (int r : new Randoms(90, 100)) {
+            System.out.println("Случайное число: " + r);
+            if (r == 100) {
+                System.out.println("Выпало число 100, давайте на этом закончим");
+                break;
+            }
+        }
+    }
+}
